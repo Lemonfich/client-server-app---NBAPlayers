2ac1257cd344048a3af58ba7d52b480142433e99
Sergei Mahov <Lemonfich@gmail.com>
2022-10-20 20:52:31 +0700

    Initial Commit

---
+++ client server app - NBAPlayers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  client server app - NBAPlayers
+//
+//  Created by Сергей on 20.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
